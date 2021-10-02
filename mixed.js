ef2455fae9c39e7799dba2476772e2a0b2c12d46
Joe Lim <joelimyx@gmail.com>
2021-10-01 21:28:19 -0400

    Mutation

--- mixed.js
+++ mixed.js
@@ -25,7 +25,13 @@ const peopleFactory = (blood, isPositive) => {
             },
             set trait(trait){
                   this._trait = trait;
+            },
+            mutate(){
+                  this._isPositive = !this._isPositive;
+                  this._blood = bloodType[Math.floor(Math.random()*bloodType.length)];
+                  this._trait = determineTrait(this._blood, this._isPositive);
             }
+            
       }
 }
 
@@ -69,3 +75,12 @@ const determineTrait = (blood, isPositive) => {
 
 let jane = peopleFactory(bloodType[1], true);
 
+console.log(`You were ${jane.trait[0]} and ${jane.trait[1]}.`);
+
+let rand = Math.floor(Math.random()*10);
+
+for (let i = 0; i < rand; i++) {
+      jane.mutate();
+}
+
+console.log(`But you've change. Now you are ${jane.trait[0]} and ${jane.trait[1]}.`);
