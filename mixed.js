75b95d0246bce42fdd260d306c2df189175b0e4a
Joe Lim <joelimyx@gmail.com>
2021-10-01 21:02:41 -0400

    Trait determined

--- mixed.js
+++ mixed.js
@@ -1,11 +1,11 @@
 
 const bloodType = ['A', 'B', 'AB', 'O'];
 
-const peopleFactory = (blood, isPositive, trait) => {
+const peopleFactory = (blood, isPositive) => {
       return{
             _blood: blood,
             _isPositive: isPositive,
-            _trait: trait,
+            _trait: determineTrait(blood, isPositive),
             get blood(){
                   return this._blood;
             },
@@ -30,7 +30,43 @@ const peopleFactory = (blood, isPositive, trait) => {
 }
 
 const determineTrait = (blood, isPositive) => {
+      let trait = [];
+      if(isPositive){
+            switch(blood){
+                  case 'A':
+                        trait.push('Earnest', 'Neat');
+                        break;
+                  case 'B':
+                        trait.push('Passionate', 'Creative');
+                        break;
+                  case 'AB':
+                        trait.push('Talented', 'Composed');
+                        break;
+                  case 'O':
+                        trait.push('Easygoing', 'A Leader');
+                        break;   
 
+            }
+      }else{
+            switch(blood){
+                  case 'A':
+                        trait.push('Stubborn', 'Anxious');
+                        break;
+                  case 'B':
+                        trait.push('Selfish', 'Uncooperative');
+                        break;
+                  case 'AB':
+                        trait.push('Eccentric', 'Two-faced');
+                        break;
+                  case 'O':
+                        trait.push('Insensitive', 'Unpunctual');
+                        break;   
+
+            }
+      }
+      return trait;
 }
 
-let jane = peopleFactory(bloodType[1], true, trait);
+let jane = peopleFactory(bloodType[1], true);
+
+console.log(jane.trait);
